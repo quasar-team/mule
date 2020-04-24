99aa4221c917e0fbdb0d5e9a250d62eaa0fd6e1c
Paris Moschovakos <paris.moschovakos@cern.ch>
2020-04-24 11:06:41 +0200

    added headers

---
+++ include/SnmpDefinitions.h
@@ -0,0 +1,109 @@
+/* 
+ * @author:     Paris Moschovakos <paris.moschovakos@cern.ch>
+ * 
+ * @copyright:  2020 CERN
+ * 
+ * @license:
+ * LICENSE:
+ * Copyright (c) 2020, CERN
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT  HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS  OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED
+ * WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL,  SPECIAL, EXEMPLARY, OR  CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT  NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS OR  SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY  THEORY  OF  LIABILITY,   WHETHER IN  CONTRACT, STRICT  LIABILITY,  OR  TORT
+ * (INCLUDING  NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY OUT OF  THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+#pragma once
+
+#include <string>
+
+namespace Snmp
+{
+
+	namespace Constants
+	{
+
+	std::string const ROOT_OID = "1.3.6.1.4.1.16394.2.1.1.";
+
+	std::string const BOARD_OID = ROOT_OID + "32.1";
+	std::string const FANTRAY_OID = ROOT_OID + "33.1";
+	std::string const POWERSUPPLY_OID = ROOT_OID + "34.1";
+	std::string const SHELFMANAGER_OID = ROOT_OID + "35.1";
+	std::string const XTCAPEM_OID = ROOT_OID + "43.1";
+	std::string const XTCAPOWERSUPPLY_OID = ROOT_OID + "44.1";
+
+	std::string const IPMC_OID = ROOT_OID + "1.1";
+	std::string const SENSOR_OID = ROOT_OID + "3.1";
+
+	int const SNMP_TIMEOUT = 1000000;
+	int const SNMP_MAX_RETRIES = 2;
+
+    enum SecurityLevel
+    {
+    	NO_AUTH_NO_PRIV = 1,
+		AUTH_NO_PRIV = 2,
+		AUTH_PRIV = 3
+    };
+
+    enum PollingGroupSpeed
+    {
+    	SLOW = 0,
+    	FAST = 1
+    };
+
+    enum Pdu
+    {
+        GET = 0,
+        SET = 1,
+        GET_NEXT = 2,
+        GET_BULK = 3,
+        TRAP = 4,
+        INFORM = 5
+    };
+
+    enum Errors
+    {
+        NO_SUCH_NAME = 0,
+        BAD_VALUE = 1,
+        TOO_BIG = 2,
+        GENERIC_ERROR = 3,
+        // SNMP v2 +
+        WRONG_VALUE = 4,
+        WRONG_ENCODING = 5,
+        WRONG_TYPE = 6,
+        WRONG_LENGTH = 7,
+        INCONSISTENT_VALUE = 8,
+        NO_ACCESS = 9,
+        NOT_WRITABLE = 10,
+        NO_CREATION = 11,
+        INCONSISTENT_NAME = 12,
+        RESOURCE_UNAVAILABLE = 13,
+        COMMIT_FAILED = 14,
+        UNDO_FAILED = 15
+
+    };
+
+    enum Exceptions
+    {
+        NO_SUCH_OBJECT = 0,
+        NO_SUCH_INSTANCE = 1
+    };
+
+  }
+
+}
