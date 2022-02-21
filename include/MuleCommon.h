4823f3db22b5c2ea404408cd0f9a0fc9015f280b
Paris Moschovakos <paris.moschovakos@cern.ch>
2022-02-21 15:20:51 +0100

    take delim as reference

--- include/MuleCommon.h
+++ include/MuleCommon.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <string>
+#include <sstream>
 namespace mule
 {
 
     template <template <typename, typename...> class ContainerType,
             typename ValueType, typename... Args>
-    std::string printContainer(const ContainerType<ValueType, Args...>& c, const std::string delim = " ") {
+    std::string printContainer(const ContainerType<ValueType, Args...>& c, const std::string& delim = " ") {
     std::string stringConcat;
     for (const auto& v : c) {
         stringConcat.append(v);
